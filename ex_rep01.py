09420fb4a002c4242e63cce55172fb1b87381b44
Gustavo Fontenele <gustavoafo171@gmail.com>
2022-06-13 20:19:29 -0300

    ex_rep01 e ex_rep02

--- ex_rep01.py
+++ ex_rep01.py
@@ -2,3 +2,10 @@
 Faça um programa que determine e mostre os cinco primeiros múltiplos de 3,
 considerando números maiores que 0.
 """
+lista = []
+n = 0
+while n < 15:
+    n = n + 3
+    lista.append(n)
+
+print(lista)
---
+++ ex_rep02.py
@@ -0,0 +1,14 @@
+"""
+Escreva um program que escreva na tela de 1 até 20, de 1 em 1. Faça 2 vezes usando as estruturas de
+repetição 'for' e 'while'.
+"""
+print("================ Contagem usando 'for' ================")
+for i in range(1, 21):
+    print(i)
+
+print("\n================ Contagem usando 'while' ================")
+x = 1
+while x <= 20:
+    print(x)
+    x += 1
+
